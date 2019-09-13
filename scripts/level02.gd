1071baff139d022bedc9b61bb026d13c08aca3c8
Gerald Schmidt <gerald1248@gmail.com>
2019-09-13 19:59:42 +0100

    difficulty alignment for level 2

--- scripts/level02.gd
+++ scripts/level02.gd
@@ -1,6 +1,6 @@
 extends TileMap
 
-const towers_n = [Vector2(15.0, 4.0), Vector2(-20.0, 10.0)]
+const towers_n = [Vector2(15.0, 4.0), Vector2(-13.0, 4.0)]
 const bricks = [Vector2(-2, 2), Vector2(-2, 3)]
 const player_start = Vector2(-0.5, 0)
 const weight_start = Vector2(-20.0, 14.0)
